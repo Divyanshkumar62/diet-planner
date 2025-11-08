ecd301c1e08e6d32412cdc9c5b756f004d3ca409
Divyansh Kumar <kumardivyansh62@gmail.com>
2025-11-08 23:51:47 +0530

    adding custom bottom nav

---
+++ components/CustomBottomNav.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import { Animated, StyleSheet, TouchableOpacity, View } from 'react-native';
+
+// Creating interface for navitem
+export interface NavItem {
+    id: string;
+    icon: string;
+    acitveIcon: string;
+    label: string;
+}
+
+interface CustomBottomNavProps {
+    items: NavItem[];
+    activeTab: string;
+    onTabPress: (id: string) => void;
+    backgroundColor: string;
+    activeBackgroundColor: string;
+    iconColor: string;
+    activeIconColor: string;
+}
+
+export const CustomBottomNav: React.FC<CustomBottomNavProps> = ({
+    items,
+    activeTab,
+    onTabPress,
+    backgroundColor = 'white',
+    activeBackgroundColor = '#f1e3ec',
+    iconColor = '#666',
+    activeIconColor = '#333'
+}) => {
+    return (
+        <View>
+            {
+                items.map((item, index) => {
+                    const isActive = activeTab === item.id;
+
+                    return (
+                        <TouchableOpacity
+                            key={}
+                        >
+                            <Animated.View>
+                                <Ionicons />
+                            </Animated.View>
+                        </TouchableOpacity>
+                    )
+                })
+            }
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    bottomNav: {
+        flexDirection: 'row',
+        paddingVertical: 12,
+        paddingHorizontal: 20,
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: -2,
+        },
+        shadowOpacity: 0.1,
+        shadowRadius: 8,
+        elevation: 10
+    },
+    navItem: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        paddingVertical: 8,
+    },
+    iconContainer: {
+        padding: 8,
+        borderRadius: 12
+    }
+});
+
+export const NAV_ITEMS: NavItem[] = [
+    {
+        id: "home",
+        icon: "home-outline",
+        acitveIcon: "home",
+        label: "Home",
+    }, 
+    {
+        id: "meals",
+        icon: "restaurant-outline",
+        acitveIcon: "restaurant",
+        label: "Meals",
+    }, 
+    {
+        id: "profile",
+        icon: "person-outline",
+        acitveIcon: "person",
+        label: "Profile",
+    }, 
+    {
+        id: "settings",
+        icon: "settings-outline",
+        acitveIcon: "settings",
+        label: "Settings",
+    }
+]
